8860a242f1a96cd1d0ea78d1c0a2509f3ad5753f
Robert P <porobertdev@gmail.com>
2024-10-30 06:57:20 +0200

    Whitelist `localhost` frontend for CORS policy

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -2,6 +2,11 @@ const { Router } = require('express');
 const rootController = require('../controllers/rootController');
 
 const rootRouter = Router();
+rootRouter.use('/api/', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4321');
+    res.setHeader('Access-Control-Allow-Headers', 'Accept,Content-Type');
+    next();
+})
 rootRouter.get('/api/pastes/:pasteID', rootController.get);
 rootRouter.post('/api/pastes/', rootController.post);
 
